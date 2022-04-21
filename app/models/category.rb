b4da7bc093b0ca24c7d8c0f82bb9f640b9de6984
dimasadh <dimasadh24@gmail.com>
2022-04-21 12:53:40 +0700

    Add validation for Category, has a valid name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :menus
+  validates :name, presence: true, uniqueness: true
 end
--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :category do
-    name { "MyString" }
+    name { "Makanan" }
   end
 end
