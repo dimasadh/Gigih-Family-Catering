077ddffad8ff97aa40781f74fa5ac7d3fea24274
dimasadh <dimasadh24@gmail.com>
2022-04-21 19:12:46 +0700

    Add validation for Menu, must be greater than or equal to 0.01

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,4 +2,5 @@ class Menu < ApplicationRecord
   belongs_to :category
   has_many :order_details
   has_many :orders, through: :order_details
+  validates :price, comparison: { greater_than_or_equal_to: 0.01 }
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Menu, type: :model do
     Category.create(name: "Kategori1")
     expect(menu = FactoryBot.build(:menu)).to be_valid
   end
+
+  it "is invalid with price lower than 0.01" do
+    menu = FactoryBot.build(:invalid_menu)
+    menu.valid?
+
+    expect(menu.errors[:price]).to include("must be greater than or equal to 0.01")
+  end
 end
