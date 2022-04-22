651e201672631fc253b5c5bff542a4f1d094271c
dimasadh <dimasadh24@gmail.com>
2022-04-23 00:17:06 +0700

    Clear unnecessary code for customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,21 +6,17 @@ class CustomersController < ApplicationController
     @customers = Customer.all
   end 
 
-  # GET /customers or /customers.json
   def index
     @customers = Customer.all
   end
 
-  # GET /customers/1 or /customers/1.json
   def show
   end
 
-  # GET /customers/new
   def new
     @customer = Customer.new
   end
 
-  # GET /customers/1/edit
   def edit
   end
 
@@ -30,52 +26,40 @@ class CustomersController < ApplicationController
     redirect_to menus_path
   end
 
-  # POST /customers or /customers.json
   def create
     @customer = Customer.new(customer_params)
 
-    respond_to do |format|
-      if @customer.save
-        session[:current_customer] = @customer
-        format.html { redirect_to customer_url(@customer), notice: "Customer was successfully created." }
-        format.json { render :show, status: :created, location: @customer }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-      end
+    if @customer.save
+      session[:current_customer] = @customer
+      redirect_to customer_url(@customer), notice: "Customer was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /customers/1 or /customers/1.json
   def update
     respond_to do |format|
       if @customer.update(customer_params)
-        format.html { redirect_to customer_url(@customer), notice: "Customer was successfully updated." }
-        format.json { render :show, status: :ok, location: @customer }
+        redirect_to customer_url(@customer), notice: "Customer was successfully updated."
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity
       end
     end
   end
 
-  # DELETE /customers/1 or /customers/1.json
   def destroy
     @customer.destroy
 
     respond_to do |format|
-      format.html { redirect_to customers_url, notice: "Customer was successfully destroyed." }
-      format.json { head :no_content }
+      redirect_to customers_url, notice: "Customer was successfully destroyed."
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_customer
       @customer = Customer.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def customer_params
       params.require(:customer).permit(:email)
     end
