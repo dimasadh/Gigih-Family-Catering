41ab0bf0d428c7da7f497b9f7246bf4b98aad464
dimasadh <dimasadh24@gmail.com>
2022-04-23 00:57:47 +0700

    Remove unnecessary code for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,72 +2,53 @@ class OrdersController < ApplicationController
   before_action :set_order, only: %i[ show edit update destroy ]
   skip_before_action :verify_authenticity_token
 
-  # GET /orders or /orders.json
   def index
     @orders = Order.where("customer_id = ?", session[:current_customer]["id"])
   end
 
-  # GET /orders/1 or /orders/1.json
   def show
     session[:current_order] = @order.id
     @order_details = OrderDetail.where("order_id = ?", params[:id])
   end
 
-  # GET /orders/new
   def new
     @order = Order.new
   end
 
-  # GET /orders/1/edit
   def edit
 
   end
 
-  # POST /orders or /orders.json
   def create
     @order = Order.new(order_params.merge(customer_id: session[:current_customer]["id"]))
 
-    respond_to do |format|
-      if @order.save
-        format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
+    if @order.save
+      redirect_to order_url(@order), notice: "Order was successfully created." 
+    else
+      render :new, status: :unprocessable_entity 
     end
   end
 
-  # PATCH/PUT /orders/1 or /orders/1.json
   def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
+    if @order.update(order_params)
+      redirect_to order_url(@order), notice: "Order was successfully updated." 
+    else
+      render :edit, status: :unprocessable_entity 
     end
   end
 
-  # DELETE /orders/1 or /orders/1.json
   def destroy
     @order.destroy
 
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to orders_url, notice: "Order was successfully destroyed." 
+   
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_order
       @order = Order.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def order_params
       params.permit(:total_price, :status)
     end
