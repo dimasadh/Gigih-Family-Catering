4cf52d74fc00594f397d0ea676eb0ac66fce3f23
dimasadh <dimasadh24@gmail.com>
2022-04-22 19:03:26 +0700

    Change Order and Cart list view into table view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   # GET /orders or /orders.json
   def index
-    @orders = Order.all
+    @orders = Order.where("customer_id = ?", session[:current_customer]["id"])
   end
 
   # GET /orders/1 or /orders/1.json
