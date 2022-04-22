9fa220c5ca53fa50a6a8c4e51a3bc5f3aad61433
dimasadh <dimasadh24@gmail.com>
2022-04-22 23:37:40 +0700

    Refactor report's views template

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,18 +9,23 @@ class ReportsController < ApplicationController
     end
 
     def report_by_date_range
+        @start_date = params[:start_date]
+        @end_date = params[:end_date]
         @orders = Order.where(created_at: order_params[:start_date]..order_params[:end_date])
     end
 
     def report_by_customer
         @orders = Order.where(customer_id: order_params[:customer][:id])
+        @customer = Customer.find(order_params[:customer][:id])
     end
 
     def report_by_greater_than
+        @price = order_params[:price]
         @orders = Order.where("total_price > ?", order_params[:price]).order(total_price: :desc)
     end
 
     def report_by_lower_than
+        @price = order_params[:price]
         @orders = Order.where("total_price < ?", order_params[:price]).order(total_price: :asc)
     end 
 
